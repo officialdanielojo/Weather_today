60f53f6bd543fd0b26ceb8aceda8ccfbbfa3377d
Ojo Daniel <danielojo237@gmail.com>
2025-11-07 15:24:22 +0100

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
-import { Analytics } from '@vercel/analytics/next'
+// import { Analytics } from '@vercel/analytics/next'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
+    {/* <Analytics /> */}
   </StrictMode>
 )
